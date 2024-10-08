584b1bbdc79d0dcb331bd1e24d48b64196ee0f4d
imhl <25532076+imhl@users.noreply.github.com>
2024-10-09 01:18:26 +0800

    Update App.js

--- src/App.js
+++ src/App.js
@@ -22,12 +22,12 @@ function App() {
         <Router>
           <nav>
             <div className="d-flex justify-content-center mt-5 transparent-border">
-              <Link to="/STX" className="btn btn-primary me-2"><i className="bi bi-graph-up"></i> STX</Link>
+              <Link to="/STX" className="btn btn-primary me-2"><i className="bi bi-graph-up"></i> STX12345</Link>
               <Link to="/OM" className="btn btn-secondary"><i className="bi bi-toggles2"></i> OM</Link>
             </div>
           </nav>
           <Routes>
-            <Route path="/STXXX" element={<ShowPage />} />
+            <Route path="/STX" element={<ShowPage />} />
             <Route path="/OM" element={<ShowPageOM />} />
           </Routes>
         </Router>
